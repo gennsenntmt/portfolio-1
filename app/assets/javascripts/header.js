24b4eab6344ba5c76e5572d9dc452a473a026c08
gennsenntmt <56209451+gennsenntmt@users.noreply.github.com>
2019-12-12 18:45:46 +0900

    login-sign-up -view

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,24 @@
+
+// function showTime(){
+//   let date = new Date(); // 任意の日付を入力
+//   let year = date.getFullYear();
+//   let month = date.getMonth() + 1;
+//   let day = date.getDate();
+//   let week_japanese = ["日", "月", "火", "水", "木", "金", "土"];
+//   let week = week_japanese[date.getDay()];
+//   let hour = date.getHours();
+//   let minute = date.getMinutes();
+//   let second = date.getSeconds();
+
+//   $("#date-time").text(`${year}/${month}/${day}/(${week}) ${hour}:${minute}`);
+// };
+//   setInterval('showTime()',1000);
+
+
+
+
+
+
+
+
+
