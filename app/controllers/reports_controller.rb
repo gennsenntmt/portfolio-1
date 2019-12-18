dcad7e5a31d1891a2e5a080ed20e9a16210df64a
gennsenntmt <56209451+gennsenntmt@users.noreply.github.com>
2019-12-18 16:40:08 +0900

    report-edit-destroy

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,9 @@
 class ReportsController < ApplicationController
   def index
-    # @reports = Report.all.order("created_at DESC")
+    @reports = Report.all.order("created_at DESC")
     @students = Student.all.order("created_at DESC")
-
-    @q = Report.ransack(params[:q])
-    @reports = @q.result(distinct: true)
+    # @q = Report.ransack(params[:q])
+    # @reports = @q.result(distinct: true)
   end
 
   def search
@@ -14,6 +13,8 @@ class ReportsController < ApplicationController
 
   def show
     @report = Report.find(params[:id])
+    @user = current_user
+    @student_all = Student.all
   end
 
   def new
@@ -23,7 +24,6 @@ class ReportsController < ApplicationController
   end
 
   def create
-    
     @report = Report.new(report_params)
     if @report.save
       redirect_to root_path
@@ -36,9 +36,22 @@ class ReportsController < ApplicationController
   end
 
   def update
+    @report = Report.find(params[:id])
+    if @report.update(report_params) 
+      redirect_to root_path
+    else       
+      redirect_to report_path(@report)
+    end
   end
 
   def destroy
+    @report = Report.find(params[:id])
+    if @report.destroy
+      redirect_to root_path
+    else
+      flash[:notice] = '問題が発生して削除できませんでした'
+      render :show
+    end
   end
 
 
@@ -51,7 +64,7 @@ class ReportsController < ApplicationController
           .merge(user_id: current_user.id)
   end
 
-  def search_params
-    params.require(:q).permit(:title_cont)
-  end
+  # def search_params
+  #   params.require(:q).permit(:title_cont)
+  # end
 end
